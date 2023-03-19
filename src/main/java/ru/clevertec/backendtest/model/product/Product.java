91e0f926bec74bfe8f08be46b0a20fe16050d4d1
VladislavSkrupski <vladislav.skrupski@gmail.com>
2023-03-19 23:14:04 +0300

    refactor: refactor with using Lombok, change way of work with program arguments

--- src/main/java/ru/clevertec/backendtest/model/product/Product.java
+++ src/main/java/ru/clevertec/backendtest/model/product/Product.java
@@ -1,38 +1,15 @@
 package ru.clevertec.backendtest.model.product;
 
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 public abstract class Product implements Goods {
     private final int id;
     @Pattern(regexp = "^\\S+[a-zA-Zа-яА-ЯёЁ\\s\\d-()/,]{3,}\\S+$")
     private final String name;
     private final double price;
     private final boolean isPromotional;
-
-    public Product(int id, String name, double price, boolean isPromotional) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.isPromotional = isPromotional;
-    }
-
-    @Override
-    public int getId() {
-        return id;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public double getPrice() {
-        return price;
-    }
-
-    @Override
-    public boolean isPromotional() {
-        return isPromotional;
-    }
 }
--- src/main/java/ru/clevertec/backendtest/model/product/Units.java
+++ src/main/java/ru/clevertec/backendtest/model/product/Units.java
@@ -1,17 +1,14 @@
 package ru.clevertec.backendtest.model.product;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Units {
     BULK("кг"),
     LIQUID("л"),
     PIECE("шт.");
 
     private final String unit;
-
-    Units(String unit) {
-        this.unit = unit;
-    }
-
-    public String getUnit() {
-        return unit;
-    }
 }
